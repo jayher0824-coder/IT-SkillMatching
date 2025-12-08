1c7ede5a1e15c751e7fb8d83a8b51fcefe506495
jayher0824-coder <jayher0824@gmail.com>
2025-12-09 03:04:13 +0800

    Add feedback_response to notification type enum - fixes feedback response notification error

--- server/database/models/Notification.js
+++ server/database/models/Notification.js
@@ -17,6 +17,7 @@ const notificationSchema = new mongoose.Schema({
       'assessment_reminder',
       'job_posted',
       'feedback_received',
+      'feedback_response',
       'retake_approved',
       'retake_rejected',
       'system'
